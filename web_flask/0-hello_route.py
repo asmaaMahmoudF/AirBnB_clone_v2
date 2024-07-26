2ad018e320cfd406d3000de20809a3e7f3a308c9
Mohamedaley <vulturepremier@gmail.com>
2024-07-26 15:06:08 +0300

    New commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+
+
 """
 starts a Flask web application
 """
@@ -11,6 +13,7 @@ app = Flask(__name__)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
